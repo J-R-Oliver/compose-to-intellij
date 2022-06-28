0c3d4e1ed4028f9678f83d1d6e0fa044754ca98d
James Oliver <joliver.robert@me.com>
2022-06-28 17:23:57 +0100

    feat: updates description with arguments text

--- cmd/compose-to-intellij/main.go
+++ cmd/compose-to-intellij/main.go
@@ -10,7 +10,7 @@ func main() {
 
 	program.
 		Name("converter").
-		Description("Command line application to convert Docker Compose environment variables to IntelliJ format.").
+		Description("Command line application to convert Docker Compose environment variables to IntelliJ format. Optional arguments of compose service names can be passed to filter the output.").
 		Version("1.0.0").
 		Option("i", "input", "input", "filepath for docker-compose YAML file", "./docker-compose.yml").
 		Action(internal.Action).
